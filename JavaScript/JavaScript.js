aabbdaa612fbb18de881a8c5f7a3ec64d12e84d8
Mahfujur Rahman <106644950+Mahfujur-Rahman-0@users.noreply.github.com>
2023-09-04 10:56:52 +0600

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -3,7 +3,7 @@ let title = document.getElementById("title");
 let getMemeBtn = document.getElementById("get-meme-btn");
 
 
-let url = "https://meme-api.com/gimme";
+let url = "https://meme-api.com/gimme/";
 
 let subreddits = ["catmemes","wholesomes","dogmemes","me_irl"];
 
